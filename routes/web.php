48cfbb401eff4122b0a7a5e65bfcb7c0801660af
muhammadzakyalfarizi060605 <muhammadzakyalfarizi060605@gmail.com>
2024-09-06 14:07:57 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+
+Route::resource('photos', PhotoController::class);
 
 // Route::middleware(['auth','admin'])->group(function(){
 //     Route::get('/master/book',function(){
@@ -30,6 +38,17 @@ use Illuminate\Support\Facades\Route;
 //     // hasil url: localhost/PWL-2024/public/admin/event
 // });
 
-// Route::redirect('/here','/there');
-Route::view('/welcome', 'welcome');
-Route::view('/welcome', 'welcome', ['name'=>'Taylor']);
+// // Route::redirect('/here','/there');
+// Route::view('/welcome', 'welcome');
+// Route::view('/welcome', 'welcome', ['name'=>'Taylor']);
+
+// Route::get('/', [HomeController::class, 'index']);
+// Route::get('/about', [AboutController::class, 'about']);
+// Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+Route::resource('photos', PhotoController::class)->only([
+  'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+  'create', 'store', 'update', 'destroy'
+]);
